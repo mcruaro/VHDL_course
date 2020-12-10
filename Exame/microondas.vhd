e12db4658908223afa56634259d8f87001f88c4b
Marcelo Ruaro <mcruaro@gmail.com>
2020-12-10 15:52:09 -0300

    Adicao Exame - projeto base

---
+++ Exame/microondas.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity microondas is
+    port (
+        --Entrada
+        clock : in std_logic;
+        reset : in std_logic;
+        --Entradas
+
+        --Saidas
+    );
+end microondas;
+
+architecture microondas of microondas is
+    
+    --Continuar
+        
+end architecture;
