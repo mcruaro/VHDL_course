588c9ea4282c1def4936d3a822fe29796672da7a
Marcelo Ruaro <marceloruaro@setrem.com.br>
2021-08-21 01:11:27 +0200

    Primeiro exemplo

---
+++ Exemplo/simple_example.vhd
@@ -0,0 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity simple_example is
+    port (
+        a : in std_logic;
+        b : in std_logic;
+        o : out std_logic
+    );
+end simple_example;
+
+architecture simple_example of simple_example is
+begin
+    o <= a and b;
+end simple_example;
